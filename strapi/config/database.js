d755f4fdc2f2faf6f8168a8e7ab7e14130d5d97b
Zaphyrzan <wzafirzan@gmail.com>
2021-08-27 17:06:18 +0800

    latest

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'blogdb'),
         username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Titanic22'),
+        password: env('DATABASE_PASSWORD', 'admin123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
